e37e7e73013d134a2d76927d0d2b6969a9937561
matintohidi <matintovhidi@gmail.com>
2025-01-03 06:28:38 +0330

    Test for github action

--- index.js
+++ index.js
@@ -8,32 +8,38 @@ const db = require("./models");
 
 db.mongoose.set("strictQuery", false);
 db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to the database!");
-    })
-    .catch(err => {
-        console.log("Cannot connect to the database!", err);
-    });
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+  });
 
 const app = express();
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
-const corsOptions ={
-    origin:'http://localhost:3000',
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
 app.use(express.static(__dirname + "/"));
-app.use("/api/auth" , userRoutes);
+app.use("/api/auth", userRoutes);
 
-app.listen(process.env.NODE_DOCKER_PORT , () => {
-    console.log("server is running successfully");
-})
+app.listen(process.env.NODE_DOCKER_PORT, () => {
+  console.log("server is running successfully");
+});
